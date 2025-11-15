72972e0fce265c6f46df78de9e8a4bcefe8d98c3
yuval123456878910 <132678531+yuval123456878910@users.noreply.github.com>
2025-11-15 11:52:09 +0200

    Delete testing.py

--- testing.py
+++
@@ -1,8 +0,0 @@
-from tokenizer import tokenizer
-from order import ordered_line
-from ast_builder import combiner
-keywords = ["int", "str", "float","func"]
-token = tokenizer("func hi int() {1+1; 2*2;};",keywords)
-ord_line = ordered_line(token)
-AST = combiner(ord_line) # the ast
-print((AST[0]))
